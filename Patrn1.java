b42b9e6947978ac34e085c8dbd291c45c5fb089e
varshithavarsh <71486605+varshithavarsh@users.noreply.github.com>
2021-07-01 20:05:56 +0530

    Add files via upload

--- Patrn1.java
+++ Patrn1.java
@@ -5,7 +5,7 @@ public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
 int n=sc.nextInt();
-for(int i=1;i<=n;i++)
+for(int i=0;i<n;i++)
 {
 for(int j=1;j<=i;j++)
 {
@@ -13,5 +13,14 @@ System.out.print("*");
 }
 System.out.println();
 }
+for(int i=n-1;i>=0;i--)
+{
+for(int j=0;j<=i-1;j++)
+{
+System.out.print("*");
+}
+System.out.println();
 }
 }
+}
+
