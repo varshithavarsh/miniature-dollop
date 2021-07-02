e6588304251ad8d6038d834cffa3a59d6934591b
varshithavarsh <71486605+varshithavarsh@users.noreply.github.com>
2021-07-02 19:28:52 +0530

    Add files via upload

---
+++ Noptrn.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Noptrn
+{
+public static void main(String args[])
+{
+Scanner var=new Scanner(System.in);
+int n=var.nextInt();
+for(int i=1;i<=n;i++)
+{
+for(int j=1;j<=i;j++)
+{
+System.out.print(j);
+}
+System.out.println();
+}}} 
